6c5ee2c44376e360ecdcad42e6527b37cfd19e81
温馨佳园 <ijlhjj@126.com>
2021-01-18 20:53:39 +0800

    新增：项目启动和配置文件
    1、application.properties
    2、SweetmanorApplication.java
    3、ServletInitializer.java
    4、SweetmanorApplicationTests.java

---
+++ src/main/java/com/sweetmanor/SweetmanorApplication.java
@@ -0,0 +1,13 @@
+package com.sweetmanor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetmanorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetmanorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sweetmanor/SweetmanorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetmanor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetmanorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
